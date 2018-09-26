63141402abcad4207c134f74e0642e99235a3869
Arjun <arjunk@thoughtworks.com>
2018-09-26 20:47:08 +0530

    init

---
+++ rules.js
@@ -0,0 +1,11 @@
+const moment = require("moment");
+const _ = require("lodash");
+import {
+    RuleFactory,
+    FormElementsStatusHelper,
+    FormElementStatusBuilder,
+    FormElementStatus,
+    VisitScheduleBuilder
+} from 'rules-config/rules';
+
+
