dca1300da19f285e7bde2676d675b0e89799affc
Arjun <arjunk@thoughtworks.com>
2018-10-06 11:07:48 +0530

    Rules

--- rules.js
+++ rules.js
@@ -4,8 +4,62 @@ import {
     RuleFactory,
     FormElementsStatusHelper,
     FormElementStatusBuilder,
+    StatusBuilderAnnotationFactory,
     FormElementStatus,
     VisitScheduleBuilder
 } from 'rules-config/rules';
 
+const RegistrationViewFilter = RuleFactory("cc10c94c-c1b6-4957-8738-da4a3bc2ea09", "ViewFilter");
+
+const WithStatusBuilder = StatusBuilderAnnotationFactory('programEncounter', 'formElement');
+
+@RegistrationViewFilter("f24d531d-355c-403d-8325-7d2ea0661496", "Kalap Registration View Filter", 100.0, {})
+class RegistrationHandlerKalap {
+    static exec(individual, formElementGroup) {
+        console.log("came to exec");
+        console.log(formElementGroup.name);
+        return FormElementsStatusHelper
+            .getFormElementsStatusesWithoutDefaults(new RegistrationHandlerKalap(), individual, formElementGroup);
+    }
+
+    _getStatusBuilder(individual, formElement) {
+        return new FormElementStatusBuilder({individual, formElement});
+    }
+
+    @WithStatusBuilder
+    maritalStatus([], statusBuilder){
+        statusBuilder.skipAnswers('Divorced', 'Unmarried', 'Remarried', 'Other');
+    }
+
+    @WithStatusBuilder
+    casteCategory([], statusBuilder){
+        statusBuilder.skipAnswers('ST');
+    }
+
+    @WithStatusBuilder
+    economicStatus([], statusBuilder){
+        statusBuilder.skipAnswers('No');
+    }
+
+    typeOfFacilityVisitedByFamilyMembersInLast12Months(individual, formElement) {
+        const statusBuilder = this._getStatusBuilder(individual, formElement);
+        statusBuilder.show().when.valueInRegistration("Family member visited healthcare facility in last 12 months").containsAnswerConceptName("Yes");
+        return statusBuilder.build();
+    }
+
+    abc(individual, formElementGroup) {
+        console.log("came to FEG healthcareExperienceOutpatientFacility");
+        console.log(formElementGroup.formElements.length);
+        return formElementGroup.formElements.map(fe=>{
+            console.log(fe.name);
+            let statusBuilder = new FormElementStatusBuilder({individual, fe});
+        statusBuilder.show().when.valueInRegistration("Type of facility visited by family members in last 12 months").containsAnswerConceptName("Outpatient");
+        return statusBuilder.build();
+        })
+    }
+
+
+}
+
+module.exports = {RegistrationHandlerKalap}
 
